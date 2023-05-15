ec8f9e848e10df23092b92e09d5a3c548d2d5297
4Noyis <berenalpb@outlook.com>
2023-05-16 00:48:57 +0300

    basics

---
+++ script.js
@@ -0,0 +1,19 @@
+console.log("Hello World");
+
+
+function add(x,y){
+    return x+y
+}
+
+function subtract(x,y){
+    return x-y
+}
+
+function multiply(x,y){
+    return x*y
+}
+
+function divide(x,y){
+    return x/y
+}
+
